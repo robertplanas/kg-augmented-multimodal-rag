86c7062a6696ea603ed26519fcd6276645df897c
Robert Planas <robertplanas87@gmail.com>
2026-03-31 20:02:51 +0200

    populate_database refractor

--- populate_database.py
+++ populate_database.py
@@ -1,60 +1,75 @@
 import argparse
-import logging
 import glob
+import logging
+from pathlib import Path
+from typing import List, Tuple
 
-from utils.ingest import ingest_document
 from utils.database_utils import generate_database_and_retriever, populate_database
+from utils.ingest import ingest_document
 from utils.summarize import summarize_objects
 
 
 LOGGER = logging.getLogger(__name__)
+SUPPORTED_EXTENSIONS: Tuple[str, ...] = ("pdf", "ipynb")
 
-parser = argparse.ArgumentParser(description="Populate DB")
 
-parser.add_argument(
-    "-df",
-    "--document_folder",
-    type=str,
-    default=None,
-    help="Folder where the documents that need parsing are stored.",
-    required=True,
-)
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Populate DB")
+    parser.add_argument(
+        "-df",
+        "--document_folder",
+        type=str,
+        help="Folder where the documents that need parsing are stored.",
+        required=True,
+    )
+    parser.add_argument(
+        "-db",
+        "--data_base",
+        type=str,
+        default="./localdb",
+        help="Folder where the DB will be stored.",
+    )
+    return parser.parse_args()
 
-parser.add_argument(
-    "-db",
-    "--data_base",
-    type=str,
-    default="./localdb",
-    help="Folder where the DB will be stored.",
-    required=True,
-)
 
-args = parser.parse_args()
+def get_documents(folder: str) -> List[str]:
+    documents: List[str] = []
+    for extension in SUPPORTED_EXTENSIONS:
+        documents.extend(glob.glob(f"{folder}/*.{extension}"))
+    return sorted(documents)
 
-if __name__ == "__main__":
+
+def process_documents(documents: List[str]):
+    all_texts = []
+    all_tables = []
+    all_images = []
+
+    for doc in documents:
+        LOGGER.info("Parsing document: %s", doc)
+        text_objs, table_objs, image_objs = ingest_document(doc)
+        all_texts.extend(text_objs)
+        all_tables.extend(table_objs)
+        all_images.extend(image_objs)
+
+    return all_texts, all_images, all_tables
+
+
+def main() -> None:
+    args = parse_args()
     logging.basicConfig(
         level=logging.CRITICAL, format="%(name)s %(asctime)s %(message)s"
     )
     LOGGER.setLevel(logging.INFO)
 
-    folder = args.document_folder
-    assert folder is not None and folder != "", "Folder needs to be specified"
+    folder = Path(args.document_folder)
+    if not folder.exists() or not folder.is_dir():
+        raise ValueError(
+            f"Document folder does not exist or is not a directory: {folder}"
+        )
+
     LOGGER.info("Parsing supported documents in %s", folder)
-    all_documents = []
-    for extension in ("pdf", "ipynb"):
-        all_documents.extend(glob.glob(f"{folder}/*.{extension}"))
-    all_documents = sorted(all_documents)
-
-    all_texts = []
-    all_tables = []
-    all_images = []
-
-    for doc in all_documents:
-        LOGGER.info("Parsing document: {}".format(doc))
-        text_objs, table_objs, images_objs = ingest_document(doc)
-        all_texts.extend(text_objs)
-        all_tables.extend(table_objs)
-        all_images.extend(images_objs)
+    all_documents = get_documents(str(folder))
+    all_texts, all_images, all_tables = process_documents(all_documents)
 
     LOGGER.info("Generating descriptions and summaries for objects")
     all_texts, all_images, all_tables = summarize_objects(
@@ -67,4 +82,8 @@ if __name__ == "__main__":
     LOGGER.info("Populating DB")
     retriever = populate_database(retriever, all_texts, all_images, all_tables)
 
-    LOGGER.info("Sucessfully populated the DB")
+    LOGGER.info("Successfully populated the DB")
+
+
+if __name__ == "__main__":
+    main()
